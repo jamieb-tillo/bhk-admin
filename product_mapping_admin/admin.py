b7d031639ed06d1eff84f76d0305e8fe65556e88
Jamie Batabyal <jamie.batabyal@tillo.io>
2023-04-18 16:04:38 +0100

    Deleted ProductMapping model

--- product_mapping_admin/admin.py
+++ product_mapping_admin/admin.py
@@ -1,4 +1,2 @@
 from django.contrib import admin
-from .models import ProductMapping
 # Register your models here.
-admin.site.register(ProductMapping)
--- product_mapping_admin/models.py
+++ product_mapping_admin/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-
-
-class ProductMapping(models.Model):
-    product_id = models.IntegerField()
-    brand = models.CharField(max_length=255)
-    delivery_method = models.CharField(max_length=255)
